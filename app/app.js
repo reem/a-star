dbad2ef9eff684503de6eb14773c699d427f0ad7
Jonathan Reem <jonathan.reem@gmail.com>
2014-05-11 12:06:26 -0700

    Refactored to avoid needless copying of function pointers.

--- app/app.js
+++ app/app.js
@@ -1,38 +1,44 @@
-/* globals AStar, d3, console, Post, Graph, Vector, _, window */
+/* globals mori, d3, console, Vector, _, window, setTimeout */
 var App = {};
 
 (function (exports) {
   var size = 100;
-  var source = 0;
+  var start = 0;
   var goal = size - 1;
   var maxEdgeDistance = 100;
   var maxEdges = 10;
-  var padding = 50; 
+  var padding = 50;
 
   var svg;
-
   var init = function () {
-    initSvg();
+    svg = d3.select("svg")
+      .attr("width", window.innerWidth)
+      .attr("height", window.innerHeight);
+
     var locations = randomGraphLocations(size);
     animate( // While not done animate a-star
       mori.take_while(
         notDone,
         mori.iterate(
-          stepAStar, // Steps the algorithm
-          startAStarState(
-            size, // Size
-            locations, // Locations array - not passed to a-star
-            source, // Start index
-            goal, // Goal index
+          runAStar(
+            randomConnectedGraph(size, locations),
+            start,
+            function (n) {
+              return n === goal;
+            },
             function (self) { // Heuristic
               return locations[self].distance(locations[goal]);
             },
             function (self, other) { // Neighbor Distance
               return locations[self].distance(locations[other]);
             }
-          ))),
+          ),
+          initAStarState(start)
+        )
+      ),
       _.partial(animateGraphState, size, locations), // Animator
-      150); // Wait time
+      150
+    ); // Wait time
   };
 
   exports.init = init;
@@ -48,8 +54,8 @@ var App = {};
     }
   };
 
-  var notDone = function (x) { 
-    return x !== null && x.end !== null; 
+  var notDone = function (x) {
+    return x !== null && x.end === null;
   };
 
   var animateGraphState = function (size, locations, graphState) {
@@ -90,50 +96,49 @@ var App = {};
     d3Ids.exit().remove();
   };
 
-  var startAStarState = function (
-      size, locations, start, goal, heuristic, neighborDistance) {
-    return new AStarState(
-      randomConnectedGraph(size, locations),
-      start, goal, heuristic, neighborDistance,
-      start, mori.set(), mori.sorted_set(), mori.set(),
-      mori.hash_map(), mori.vector()
-    );
-  };
-
-  var stepAStar = function (state) {
-    if (mori.first(state.open)) {
-      if (mori.first(state.open) === goal) {
-        return recordUpdate(state, 'end', mori.first(state.open));
+  var runAStar = function (graph, start, goal, heuristic,
+    neighborDistance) {
+    return function (state) {
+      if (mori.first(state.open)) {
+        if (goal(mori.first(state.open))) {
+          return recordUpdate(state, {end: mori.first(state.open)});
+        } else {
+          return state;
+        }
       } else {
-        // magic
+        return null;
       }
-    } else {
-      return null;
-    }
+    };
   };
 
-  var recordUpdate = function (obj, key, val) {
+  var recordUpdate = function (obj, pairs) {
     var newObj = _.extend({}, obj);
     newObj[key] = val;
     return newObj;
   };
 
-  var AStarState = function (
-      graph, start, goal, heuristic,
-      neighborDistance, current, visited, 
-      open, closed, cameFrom, path, end) {
-    this.graph = graph; // id -> edges
-    this.start = start; // id of the start node
-    this.goal = goal; // id of the end node
-    this.heuristic = heuristic; // the heuristic function
-    this.neighborDistance = neighborDistance;
-    this.current = current;
-    this.visited = visited; // set of visited nodes.
-    this.open = open;
-    this.closed = closed;
-    this.cameFrom = cameFrom;
-    this.path = path;
-    this.end = end || null;
+  var AStarState = function (config) {
+    this.current = config.current;
+    this.visited = config.visited; // set of visited nodes.
+    this.score = config.score;
+    this.open = config.open;
+    this.closed = config.closed;
+    this.cameFrom = config.cameFrom;
+    this.path = config.path;
+    this.end = config.end;
+  };
+
+  var initAStarState = function (start) {
+    return new AStarState({
+      current: start,
+      visited: mori.set([start]),
+      score: mori.hash_map([start, 0]),
+      open: mori.sorted_set([start]),
+      closed: mori.set(),
+      cameFrom: mori.hash_map([start, null]),
+      path: mori.vector(),
+      end: null
+    });
   };
 
   var randomGraphLocations = function (size) {
@@ -161,15 +166,11 @@ var App = {};
     return _.reduce(locations, function (memo, location, otherId) {
       return fromLocation.distance(location) < maxEdgeDistance ?
         mori.conj(memo, otherId) :
-        memo;        
+        memo;
     }, mori.set());
   };
 
-  var initSvg = function () {
-    svg = d3.select("svg")
-      .attr("width", window.innerWidth)
-      .attr("height", window.innerHeight);
-  };
+  var initSvg = function () {};
 
   var between = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
